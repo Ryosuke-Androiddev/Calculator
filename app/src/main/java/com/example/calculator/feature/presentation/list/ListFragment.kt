dbc676647690fabaa2f6b0696c1ed7256cd75e41
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2022-06-03 23:31:03 +0900

    [setup] setup Fragment as layout

---
+++ app/src/main/java/com/example/calculator/feature/presentation/list/ListFragment.kt
@@ -0,0 +1,29 @@
+package com.example.calculator.feature.presentation.list
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.calculator.R
+import com.example.calculator.databinding.FragmentListBinding
+import com.example.calculator.databinding.FragmentUpdateBinding
+
+class ListFragment : Fragment(R.layout.fragment_list) {
+
+    private var _binding: FragmentListBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentListBinding.inflate(inflater, container, false)
+
+        Log.d("UpdateFragment", "UpdateFragment View Created")
+
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/example/calculator/feature/presentation/update/UpdateFragment.kt
@@ -0,0 +1,28 @@
+package com.example.calculator.feature.presentation.update
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.calculator.R
+import com.example.calculator.databinding.FragmentUpdateBinding
+
+class UpdateFragment : Fragment(R.layout.fragment_update) {
+
+    private var _binding: FragmentUpdateBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentUpdateBinding.inflate(inflater, container, false)
+
+        Log.d("UpdateFragment", "UpdateFragment View Created")
+
+        return binding.root
+    }
+}
