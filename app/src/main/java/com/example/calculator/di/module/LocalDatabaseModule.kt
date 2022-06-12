77d60cbe0083bf0f497cf880f99466a3a4f8b272
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2022-06-12 22:26:06 +0900

    [develop] setup di module for creating db

--- app/src/main/java/com/example/calculator/di/module/LocalDatabaseModule.kt
+++ app/src/main/java/com/example/calculator/di/module/LocalDatabaseModule.kt
@@ -1,11 +1,28 @@
 package com.example.calculator.di.module
 
+import android.app.Application
+import androidx.room.Room
+import com.example.calculator.feature.data.data_source.CalculationDatabase
+import com.example.calculator.feature.data.util.DataLayerConstants.CALCULATION_DATABASE_NAME
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object LocalDatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideCalculationDatabase(application: Application): CalculationDatabase {
+
+        return Room.databaseBuilder(
+            application,
+            CalculationDatabase::class.java,
+            CALCULATION_DATABASE_NAME
+        ).build()
+    }
 
 }
--- app/src/main/java/com/example/calculator/feature/data/util/DataLayerConstants.kt
+++ app/src/main/java/com/example/calculator/feature/data/util/DataLayerConstants.kt
@@ -3,4 +3,5 @@ package com.example.calculator.feature.data.util
 object DataLayerConstants {
 
     const val CALCULATION_INFO_TABLE_NAME = "calculation_info_table"
+    const val CALCULATION_DATABASE_NAME = "calculation_db"
 }
