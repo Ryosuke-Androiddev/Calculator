d3adaefb4b8769803fd805656db07fc1a123c47b
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2022-06-13 17:44:38 +0900

    [develop] setup dummy data

--- app/src/main/java/com/example/calculator/feature/presentation/list/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/example/calculator/feature/presentation/list/viewmodel/ListViewModel.kt
@@ -3,7 +3,9 @@ package com.example.calculator.feature.presentation.list.viewmodel
 import androidx.lifecycle.*
 import com.example.calculator.feature.domain.model.CalculationInfoItem
 import com.example.calculator.feature.domain.use_case.model.UseCase
+import com.example.calculator.feature.presentation.util.DummyData
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -11,5 +13,10 @@ class ListViewModel @Inject constructor(
     private val useCase: UseCase
 ): ViewModel() {
 
-    val getAllCalculation: LiveData<List<CalculationInfoItem>> = useCase.getAllCalculationInfoUseCase()
+    //val getAllCalculation: LiveData<List<CalculationInfoItem>> = useCase.getAllCalculationInfoUseCase()
+
+    private val list = DummyData.list
+    private val _getAllCalculation: LiveData<List<CalculationInfoItem>> = MutableLiveData(list)
+    val getAllCalculation: LiveData<List<CalculationInfoItem>> = _getAllCalculation
+
 }
