c35904f01acb8a0d35a766ef532f13e2f2ab635c
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2022-06-19 22:51:15 +0900

    [develop] setup Custom Dialog Fragment

--- app/src/main/java/com/example/calculator/feature/presentation/component/dialog/CustomPopUpDialogFragment.kt
+++ app/src/main/java/com/example/calculator/feature/presentation/component/dialog/CustomPopUpDialogFragment.kt
@@ -1,6 +1,30 @@
 package com.example.calculator.feature.presentation.component.dialog
 
+import android.annotation.SuppressLint
+import android.app.Dialog
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
+import com.example.calculator.databinding.CustomPopUpDialogBinding
 
 class CustomPopUpDialogFragment: DialogFragment() {
+
+    private var _binding: CustomPopUpDialogBinding? = null
+    private val binding get() = _binding!!
+
+    @SuppressLint("UseGetLayoutInflater")
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+
+        _binding = CustomPopUpDialogBinding.inflate(LayoutInflater.from(context))
+
+        val builder = AlertDialog.Builder(requireActivity())
+        builder.setView(binding.root)
+
+        binding.cancelButton.setOnClickListener {
+            dismiss()
+        }
+
+        return builder.create()
+    }
 }
