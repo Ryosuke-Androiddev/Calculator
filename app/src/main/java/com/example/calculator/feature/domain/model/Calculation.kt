29db0af42fa0ba5e6ac3c81013121691ba90d38b
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2022-06-28 23:43:19 +0900

    [develop] create SQL JOIN medium model class

---
+++ app/src/main/java/com/example/calculator/feature/domain/model/Calculation.kt
@@ -0,0 +1,13 @@
+package com.example.calculator.feature.domain.model
+
+import androidx.room.Embedded
+import androidx.room.Relation
+
+data class Calculation(
+    @Embedded val calculationContent: CalculationContent,
+    @Relation(
+        parentColumn = "contentId",
+        entityColumn = "calculationContentId"
+    )
+    val calculationInfoItem: CalculationInfoItem
+)
