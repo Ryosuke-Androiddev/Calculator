b915f3f1752b29aa20144d9357a351138e4e980b
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2022-06-29 00:01:41 +0900

    [develop] add logic at Dao and repository

--- app/src/main/java/com/example/calculator/feature/data/repository/CalculationRepositoryImpl.kt
+++ app/src/main/java/com/example/calculator/feature/data/repository/CalculationRepositoryImpl.kt
@@ -2,6 +2,7 @@ package com.example.calculator.feature.data.repository
 
 import androidx.lifecycle.LiveData
 import com.example.calculator.feature.data.data_source.CalculationDao
+import com.example.calculator.feature.domain.model.Calculation
 import com.example.calculator.feature.domain.model.CalculationInfo
 import com.example.calculator.feature.domain.repository.CalculationRepository
 
@@ -9,8 +10,14 @@ class CalculationRepositoryImpl(
     private val calculationDao: CalculationDao
 ): CalculationRepository{
 
+    // CalculationInfo
     override fun getAllCalculationInfo(): LiveData<List<CalculationInfo>> {
 
         return calculationDao.getAllCalculationInfo()
+    }
+
+    // Calculation
+    override fun getCalculation(): LiveData<Calculation> {
+        return calculationDao.getCalculation()
     }
 }
