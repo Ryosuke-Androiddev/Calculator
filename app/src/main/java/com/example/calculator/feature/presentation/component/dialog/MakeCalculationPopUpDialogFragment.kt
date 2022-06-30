7372f4e66f7a7b35437868d23431c002119b0dbb
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2022-07-01 00:38:45 +0900

    [develop] add dialog size determination function

--- app/src/main/java/com/example/calculator/feature/presentation/component/dialog/MakeCalculationPopUpDialogFragment.kt
+++ app/src/main/java/com/example/calculator/feature/presentation/component/dialog/MakeCalculationPopUpDialogFragment.kt
@@ -2,10 +2,13 @@ package com.example.calculator.feature.presentation.component.dialog
 
 import android.annotation.SuppressLint
 import android.app.Dialog
+import android.content.res.Resources
 import android.graphics.Color
+import android.graphics.Rect
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.ViewGroup
 import android.view.WindowManager
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
@@ -32,6 +35,29 @@ class MakeCalculationPopUpDialogFragment(
         dialog.window?.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
 
         return dialog
+    }
+
+    override fun onStart() {
+        super.onStart()
+        setWidthPercent(80)
+    }
+
+    private fun DialogFragment.setWidthPercent(percentage: Int) {
+        // percentage に変換する
+        val percent = percentage.toFloat() / 100
+        // 表示するリソースの取得
+        val dm = Resources.getSystem().displayMetrics
+        // ここでFragment の大きさを指定してる
+        // widthPixels, heightPixels この2つのパラメータ
+        // 上記の2つのパラメータをいじると、Fragmentのサイズが変更される
+        // left, top で、画面左上の座標にプロットされ、
+        // widthPixels, heightPixelsで、表示するFragmentの範囲を決定している
+        // この場合画面いっぱいに広げている
+        val rect = dm.run { Rect(0, 0, widthPixels, heightPixels) }
+        // これで横幅をしてしてるんか
+        val percentWidth = rect.width() * percent
+        // で、PopUpWindowのサイズを、ここで定義しているだけ
+        dialog?.window?.setLayout(percentWidth.toInt(), ViewGroup.LayoutParams.WRAP_CONTENT)
     }
 
     override fun onDestroyView() {
