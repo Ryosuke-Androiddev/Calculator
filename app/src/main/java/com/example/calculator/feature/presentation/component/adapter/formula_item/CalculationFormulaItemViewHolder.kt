dc35a735e96daa537cbb5ed05ff02fc7365beec6
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2022-07-02 16:46:56 +0900

    [develop] viewholder setup

--- app/src/main/java/com/example/calculator/feature/presentation/component/adapter/formula_item/CalculationFormulaItemViewHolder.kt
+++ app/src/main/java/com/example/calculator/feature/presentation/component/adapter/formula_item/CalculationFormulaItemViewHolder.kt
@@ -1,4 +1,15 @@
 package com.example.calculator.feature.presentation.component.adapter.formula_item
 
-class CalculationFormulaItemViewHolder {
+import androidx.recyclerview.widget.RecyclerView
+import com.example.calculator.databinding.LayoutCalculationFormulaItemBinding
+import com.example.calculator.feature.domain.model.CalculationContent
+
+class CalculationFormulaItemViewHolder(
+    private val binding: LayoutCalculationFormulaItemBinding
+): RecyclerView.ViewHolder(binding.root) {
+
+
+    fun bind(calculationContent: CalculationContent) {
+        binding.calculationFormula.text = calculationContent.answer.toString()
+    }
 }
