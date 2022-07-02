5c9decda96ef2e60dd165346a3c7e3d8833f64ad
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2022-07-02 16:22:39 +0900

    [develop] implement DiffUtil

--- app/src/main/java/com/example/calculator/feature/presentation/component/adapter/formula_item/CalculationFormulaItemDiffUtil.kt
+++ app/src/main/java/com/example/calculator/feature/presentation/component/adapter/formula_item/CalculationFormulaItemDiffUtil.kt
@@ -1,4 +1,25 @@
 package com.example.calculator.feature.presentation.component.adapter.formula_item
 
+import androidx.recyclerview.widget.DiffUtil
+import com.example.calculator.feature.domain.model.CalculationContent
+
 object CalculationFormulaItemDiffUtil {
+
+    val DIFF_UTIL = object : DiffUtil.ItemCallback<CalculationContent>() {
+
+        override fun areItemsTheSame(
+            oldItem: CalculationContent,
+            newItem: CalculationContent
+        ): Boolean {
+            return oldItem.contentId == newItem.contentId
+        }
+
+        override fun areContentsTheSame(
+            oldItem: CalculationContent,
+            newItem: CalculationContent
+        ): Boolean {
+            return oldItem == newItem
+        }
+
+    }
 }
