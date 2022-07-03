1631f6d87e63d055ef0b9275b5e05b625d27bbc4
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2022-07-03 23:07:23 +0900

    [develop] Modify Dummy Data

--- app/src/main/java/com/example/calculator/feature/presentation/util/DummyData.kt
+++ app/src/main/java/com/example/calculator/feature/presentation/util/DummyData.kt
@@ -9,109 +9,170 @@ object DummyData {
         CalculationInfo(
             1,
             "Java",
-            20220614L,
-            1L
+            20220614L
         ),
         CalculationInfo(
             2,
             "Kotlin",
-            20220614L,
-            1L
+            20220614L
         ),
         CalculationInfo(
             3,
             "Python",
-            20220614L,
-            1L
+            20220614L
         ),
         CalculationInfo(
             4,
             "JavaScript",
-            20220614L,
-            1L
+            20220614L
         ),
         CalculationInfo(
             5,
             "Ruby",
-            20220614L,
-            1L
+            20220614L
         ),
         CalculationInfo(
             6,
             "Swift",
-            20220614L,
-            1L
+            20220614L
         ),
         CalculationInfo(
             7,
             "PHP",
-            20220614L,
-            1L
+            20220614L
         ),
         CalculationInfo(
             8,
             "Haskell",
-            20220614L,
-            1L
+            20220614L
         ),
         CalculationInfo(
             9,
             "RUST",
-            20220614L,
-            1L
+            20220614L
         ),
         CalculationInfo(
             10,
             "Go",
-            20220614L,
-            1L
+            20220614L
         ),
         CalculationInfo(
             11,
             "C",
-            20220614L,
-            1L
+            20220614L
         ),
         CalculationInfo(
             12,
             "C+",
-            20220614L,
-            1L
+            20220614L
         ),
         CalculationInfo(
             13,
             "C++",
-            20220614L,
-            1L
+            20220614L
         ),
         CalculationInfo(
             14,
             "C#",
-            20220614L,
-            1L
+            20220614L
         ),
         CalculationInfo(
             15,
             "HTML",
-            20220614L,
-            1L
+            20220614L
         ),
     )
 
-    val contentList = listOf<CalculationContent>(CalculationContent(
+    val contentList = listOf<CalculationContent>(
+        CalculationContent(
         contentId = 1L,
         answer = 100L,
-        formulaProcess = listOf(
-            "20×5=",
-            "20×5=",
-            "20×5=",
-            "20×5=",
-            "20×5=",
-            "20×5=",
-            "20×5=",
-            "20×5=",
-            "20×5=",
-            "20×5=",
-        )
-    ))
+        formulaProcess = "20×5=",
+        1L
+        ),
+        CalculationContent(
+            contentId = 2L,
+            answer = 100L,
+            formulaProcess = "20×5=",
+            1L
+        ),
+        CalculationContent(
+            contentId = 3L,
+            answer = 100L,
+            formulaProcess = "20×5=",
+            1L
+        ),
+        CalculationContent(
+            contentId = 4L,
+            answer = 100L,
+            formulaProcess = "20×5=",
+            1L
+        ),
+        CalculationContent(
+            contentId = 5L,
+            answer = 100L,
+            formulaProcess = "20×5=",
+            1L
+        ),
+        CalculationContent(
+            contentId = 6L,
+            answer = 100L,
+            formulaProcess = "20×5=",
+            1L
+        ),
+        CalculationContent(
+            contentId = 7L,
+            answer = 100L,
+            formulaProcess = "20×5=",
+            1L
+        ),
+        CalculationContent(
+            contentId = 8L,
+            answer = 100L,
+            formulaProcess = "20×5=",
+            1L
+        ),
+        CalculationContent(
+            contentId = 9L,
+            answer = 100L,
+            formulaProcess = "20×5=",
+            1L
+        ),
+        CalculationContent(
+            contentId = 10L,
+            answer = 100L,
+            formulaProcess = "20×5=",
+            1L
+        ),
+        CalculationContent(
+            contentId = 11L,
+            answer = 100L,
+            formulaProcess = "20×5=",
+            1L
+        ),
+        CalculationContent(
+            contentId = 12L,
+            answer = 100L,
+            formulaProcess = "20×5=",
+            1L
+        ),
+        CalculationContent(
+            contentId = 13L,
+            answer = 100L,
+            formulaProcess = "20×5=",
+            1L
+        ),
+        CalculationContent(
+            contentId = 14L,
+            answer = 100L,
+            formulaProcess = "20×5=",
+            1L
+        ),
+        CalculationContent(
+            contentId = 15L,
+            answer = 100L,
+            formulaProcess = "20×5=",
+            1L
+        ),
+    )
 }
