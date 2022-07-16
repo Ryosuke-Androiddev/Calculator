96d539826bcada48cf6acc88dc28674ec2169de6
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2022-07-16 23:36:40 +0900

    [develop] reset everything and delete noneed class

--- app/src/main/java/com/example/calculator/feature/presentation/ui/add/MakeNewCalculationFragment.kt
+++ app/src/main/java/com/example/calculator/feature/presentation/ui/add/MakeNewCalculationFragment.kt
@@ -11,17 +11,15 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.annotation.ColorRes
 import androidx.core.content.ContextCompat
+import androidx.dynamicanimation.animation.SpringAnimation
+import androidx.dynamicanimation.animation.SpringForce
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.calculator.R
 import com.example.calculator.databinding.FragmentMakeNewCalculationBinding
 import com.example.calculator.feature.presentation.component.adapter.formula_item.CalculationFormulaItemAdapter
-import com.example.calculator.feature.presentation.component.gesture.FingerGestureListener
 import com.example.calculator.feature.presentation.util.DummyData
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 @AndroidEntryPoint
 class MakeNewCalculationFragment : Fragment(R.layout.fragment_make_new_calculation) {
@@ -43,62 +41,13 @@ class MakeNewCalculationFragment : Fragment(R.layout.fragment_make_new_calculati
 
         //setupRecyclerView()
 
-        binding.calculationFormulaRecyclerview.setOnTouchListener(FingerGestureListener(flickListener))
-
-        // 計算ボタンでジェスチャーを検出しても，何も操作を行わない
         binding.calculationButtonConstraintLayout.setOnTouchListener(
-            View.OnTouchListener{ view, event ->
-                when(event.action) {
-                    MotionEvent.ACTION_DOWN -> {
-                    }
-                }
+            View.OnTouchListener { view, event ->
                 return@OnTouchListener true
             }
         )
 
         return binding.root
-    }
-
-    private val flickListener = object : FingerGestureListener.Listener {
-
-        override fun onTouchScreen() {
-
-        }
-
-        override fun onFlickToTop() = setFlickToTopResult()
-
-        override fun onFlickToBottom() = setFlickToBottomResult()
-
-    }
-
-    private fun setFlickToTopResult() {
-        toggleVisible()
-    }
-
-    private fun setFlickToBottomResult() {
-        toggleInVisible()
-    }
-
-    private fun toggleVisible() {
-        binding.makeNewCalculationTitleTextView.visibility = View.VISIBLE
-        binding.calculationButtonDivider.visibility = View.VISIBLE
-        binding.calculationButtonConstraintLayout.visibility = View.VISIBLE
-    }
-
-    private fun toggleInVisible() {
-        binding.makeNewCalculationTitleTextView.visibility = View.INVISIBLE
-        binding.calculationButtonDivider.visibility = View.INVISIBLE
-        binding.calculationButtonConstraintLayout.visibility = View.INVISIBLE
-    }
-
-    private fun View.setViewBackgroundColor(@ColorRes colorId: Int) =
-        setBackgroundColor(ContextCompat.getColor(requireContext(), colorId))
-
-    private fun setupRecyclerView() {
-
-        binding.calculationFormulaRecyclerview.adapter = adapter
-        binding.calculationFormulaRecyclerview.layoutManager = LinearLayoutManager(context)
-        adapter.submitList(DummyData.contentList)
     }
 
     override fun onDestroyView() {
