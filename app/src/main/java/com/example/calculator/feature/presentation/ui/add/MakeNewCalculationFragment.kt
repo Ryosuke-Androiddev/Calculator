e318f9196a8cd05879d545af41e4caaa01ab0019
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2022-07-18 18:20:13 +0900

    [develop] nothing change

--- app/src/main/java/com/example/calculator/feature/presentation/ui/add/MakeNewCalculationFragment.kt
+++ app/src/main/java/com/example/calculator/feature/presentation/ui/add/MakeNewCalculationFragment.kt
@@ -10,6 +10,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.annotation.ColorRes
+import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.core.content.ContextCompat
 import androidx.dynamicanimation.animation.SpringAnimation
 import androidx.dynamicanimation.animation.SpringForce
