38e21d3ef1bb50722544eae52dd6bd1ff5031494
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2022-07-31 23:46:54 +0900

    [develop] create custom motionlayout class

---
+++ app/src/main/java/com/example/calculator/feature/presentation/component/motionlayout/CustomMotionLayout.kt
@@ -0,0 +1,13 @@
+package com.example.calculator.feature.presentation.component.motionlayout
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.motion.widget.MotionLayout
+
+class CustomMotionLayout(
+    context: Context,
+    attributeSet: AttributeSet? = null
+): MotionLayout(context, attributeSet) {
+
+
+}
