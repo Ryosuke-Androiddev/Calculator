2d2eb616770fac8e02c38d054a3457a8265ca304
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2022-08-02 14:13:17 +0900

    [develop] implement listener

--- app/src/main/java/com/example/calculator/feature/presentation/component/motionlayout/CustomMotionLayout.kt
+++ app/src/main/java/com/example/calculator/feature/presentation/component/motionlayout/CustomMotionLayout.kt
@@ -37,6 +37,40 @@ class CustomMotionLayout(
         // イベントが呼ばれたときに，イベントの通知を追加するためのリスナー
         addTransitionListener(object : MotionLayout.TransitionListener {
 
+            // Transition が始まった時点で呼び出される
+            override fun onTransitionStarted(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int
+            ) {
+                // Event の追加をするリスナーなので，具体的な処理はここで記述しない
+            }
+
+            // 指の位置? が移動したときに呼び出される
+            override fun onTransitionChange(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int,
+                progress: Float
+            ) {
+                // Event の追加をするリスナーなので，具体的な処理はここで記述しない
+            }
+
+            // MotionLayout が完了した時点で呼び出される処理
+            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
+                // Event の追加をするリスナーなので，具体的な処理はここで記述しない
+            }
+
+            // MotionLayoutが呼び出されるときに呼ばれる処理
+            override fun onTransitionTrigger(
+                motionLayout: MotionLayout?,
+                triggerId: Int,
+                positive: Boolean,
+                progress: Float
+            ) {
+                // Event の追加をするリスナーなので，具体的な処理はここで記述しない
+            }
+
         })
 
         // MotionLayout のイベントが，通知されるリスナーをここで追加する
@@ -44,18 +78,53 @@ class CustomMotionLayout(
         // super である必要がないので，このままにしておくか
         setTransitionListener(object : MotionLayout.TransitionListener {
 
+            override fun onTransitionStarted(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int
+            ) {
+            }
+
+            override fun onTransitionChange(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int,
+                progress: Float
+            ) {
+                transitionListenerList.filterNotNull()
+                    .forEach { transitionListener ->
+                        transitionListener.onTransitionChange(motionLayout, startId, endId, progress)
+                    }
+            }
+
+            // 処理が完了したときに呼び出される
+            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
+                transitionListenerList.filterNotNull()
+                    .forEach { transitionListener ->
+                        transitionListener.onTransitionCompleted(motionLayout, currentId)
+                    }
+            }
+
+            override fun onTransitionTrigger(
+                motionLayout: MotionLayout?,
+                triggerId: Int,
+                positive: Boolean,
+                progress: Float
+            ) {
+            }
+
         })
     }
 
     // こっちは，下の処理に，listener を渡すだけだから大きな変更がないから，superで呼び出してんのかな?
     // こいつの実装は，配列にリスナーをぶち込むことだったから，ここでの処理は，下のメソッドに引数を渡すこと
     override fun setTransitionListener(listener: TransitionListener?) {
-
+        addTransitionListener(listener)
     }
 
     // 実質，こっちの処理の方法を変えているってことなのかな
     // List にイベントを追加することで，オーバーライドしてるんかな
     override fun addTransitionListener(listener: TransitionListener?) {
-
+        transitionListenerList += listener;
     }
 }
