80068812b4ca243ca3e55ff797c2bbe918177dc3
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2022-08-04 23:49:56 +0900

    [develop] reset transition listener

--- app/src/main/java/com/example/calculator/feature/presentation/ui/add/MakeNewCalculationFragment.kt
+++ app/src/main/java/com/example/calculator/feature/presentation/ui/add/MakeNewCalculationFragment.kt
@@ -53,42 +53,6 @@ class MakeNewCalculationFragment : Fragment(R.layout.fragment_make_new_calculati
         super.onViewCreated(view, savedInstanceState)
 
         setupRecyclerView()
-
-        binding.motionBase.setTransitionListener(object : MotionLayout.TransitionListener {
-
-            override fun onTransitionStarted(
-                motionLayout: MotionLayout?,
-                startId: Int,
-                endId: Int
-            ) {
-            }
-
-            override fun onTransitionChange(
-                motionLayout: MotionLayout?,
-                startId: Int,
-                endId: Int,
-                progress: Float
-            ) {
-                // この理解が足りてない
-                if (endId == R.id.end) {
-                    // 文字通りどこからどこまで遷移するかを決定できる
-                    binding.motionBase.setTransition(R.id.end, R.id.intermediate)
-                    binding.motionBase.transitionToState(R.id.intermediate)
-                }
-            }
-
-            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
-            }
-
-            override fun onTransitionTrigger(
-                motionLayout: MotionLayout?,
-                triggerId: Int,
-                positive: Boolean,
-                progress: Float
-            ) {
-            }
-
-        })
     }
 
     private fun setupRecyclerView() {
