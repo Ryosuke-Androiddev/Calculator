b2d5422e9d863c4b2e74b30bc0242f436a250365
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2022-08-05 00:17:45 +0900

    [develop] implement ideal view motion

--- app/src/main/java/com/example/calculator/feature/presentation/ui/add/MakeNewCalculationFragment.kt
+++ app/src/main/java/com/example/calculator/feature/presentation/ui/add/MakeNewCalculationFragment.kt
@@ -53,6 +53,39 @@ class MakeNewCalculationFragment : Fragment(R.layout.fragment_make_new_calculati
         super.onViewCreated(view, savedInstanceState)
 
         setupRecyclerView()
+
+        binding.motionBase.setTransitionListener(object : MotionLayout.TransitionListener {
+
+            override fun onTransitionStarted(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int
+            ) {
+            }
+
+            override fun onTransitionChange(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int,
+                progress: Float
+            ) {
+                if (startId == R.id.end) {
+                    binding.calculationFormulaRecyclerview.scrollToPosition(DummyData.contentList.size - 1)
+                }
+            }
+
+            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
+            }
+
+            override fun onTransitionTrigger(
+                motionLayout: MotionLayout?,
+                triggerId: Int,
+                positive: Boolean,
+                progress: Float
+            ) {
+            }
+
+        })
     }
 
     private fun setupRecyclerView() {
