a02d0b7e173fc82e2684dd127dbfc0dbe6f7014b
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2022-08-06 22:52:44 +0900

    [develop] remove no need import line

--- app/src/main/java/com/example/calculator/feature/presentation/ui/add/MakeNewCalculationFragment.kt
+++ app/src/main/java/com/example/calculator/feature/presentation/ui/add/MakeNewCalculationFragment.kt
@@ -6,24 +6,15 @@ import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.annotation.ColorRes
 import androidx.annotation.RequiresApi
 import androidx.constraintlayout.motion.widget.MotionLayout
-import androidx.core.content.ContextCompat
-import androidx.dynamicanimation.animation.SpringAnimation
-import androidx.dynamicanimation.animation.SpringForce
-import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.calculator.R
 import com.example.calculator.databinding.FragmentMakeNewCalculationBinding
 import com.example.calculator.feature.presentation.component.adapter.formula_item.CalculationFormulaItemAdapter
 import com.example.calculator.feature.presentation.util.DummyData
-import com.google.android.material.bottomsheet.BottomSheetBehavior
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
