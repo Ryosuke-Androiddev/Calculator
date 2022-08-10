79707270583f30e381585766047a81bae796cd1e
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2022-08-10 12:13:45 +0900

    [develop] implement button click event

--- app/src/main/java/com/example/calculator/feature/presentation/component/dialog/MakeCalculationPopUpDialogFragment.kt
+++ app/src/main/java/com/example/calculator/feature/presentation/component/dialog/MakeCalculationPopUpDialogFragment.kt
@@ -7,8 +7,10 @@ import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.WindowManager
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import com.example.calculator.databinding.MakeCalculationPopUpDialogBinding
+import com.example.calculator.feature.domain.model.CalculationInfo
 import com.example.calculator.feature.presentation.component.dialog.parent.CustomDialogFragmentParent
 
 class MakeCalculationPopUpDialogFragment(
@@ -27,6 +29,23 @@ class MakeCalculationPopUpDialogFragment(
         _binding = MakeCalculationPopUpDialogBinding.inflate(LayoutInflater.from(requireContext()))
 
         builder.setView(binding.root)
+
+        binding.saveTextButton.setOnClickListener {
+            // ここの内容を画面遷移したときに表示する??
+            // DBのテーブル結語で乗りきれいないよね??
+            // SafeArgs or Bundleで渡してあげる必要がある
+            val title = binding.calculationTitleTextview.text
+            if (title.isNullOrEmpty()) {
+                // ここの内容を差し替えて、UseCaseに渡す
+                Toast.makeText(requireContext(), "Hello world", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(requireContext(), "Please input 0 more characters", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.cancelTextButton.setOnClickListener {
+            dismiss()
+        }
 
         dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         dialog.window?.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
