375eb7fcb6f4a7c02d91f343e558d52d1bbf59cd
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2022-08-15 23:36:25 +0900

    [develop] Customize DeleteDialog Layout

--- app/src/main/java/com/example/calculator/feature/presentation/component/dialog/DeletePopUpDialogFragment.kt
+++ app/src/main/java/com/example/calculator/feature/presentation/component/dialog/DeletePopUpDialogFragment.kt
@@ -5,31 +5,41 @@ import android.app.Dialog
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.WindowManager
 import androidx.appcompat.app.AlertDialog
+import com.example.calculator.databinding.DeletePopUpDialogFragmentBinding
 import com.example.calculator.feature.presentation.component.dialog.parent.CustomDialogFragmentParent
 
 class DeletePopUpDialogFragment : CustomDialogFragmentParent() {
+
+    private var _binding : DeletePopUpDialogFragmentBinding? = null
+    private val binding get() = _binding!!
 
     private val builder by lazy { AlertDialog.Builder(requireActivity()) }
     private val dialog by lazy { builder.create() }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
 
+        _binding = DeletePopUpDialogFragmentBinding.inflate(LayoutInflater.from(context))
+        builder.setView(binding.root)
+
         super.onCreateDialog(savedInstanceState)
 
-        builder.setPositiveButton("Yes") { _, _ ->
-
-        }
-        builder.setNegativeButton("No") { _, _ ->
-
-        }
-        builder.setTitle("Delete Calculation")
-        builder.setMessage("Do you want to delete Calculation??")
-
-        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.GRAY))
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         dialog.window?.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
 
         return dialog
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        setWidthPercent(90)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
